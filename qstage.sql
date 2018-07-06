b3719a8a26abd5ae3ad4a6156dccf9ae5b041b94
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2018-07-05 22:21:23 -0300

    Adiciona script para dump do DB. Inclui modelos

--- qstage.sql
+++ qstage.sql
@@ -1,3 +1,11 @@
+-- DB QStage
 CREATE TABLE musicas( musica_id INTEGER NOT NULL PRIMARY KEY, titulo STRING NOT NULL, html TEXT, programa TEXT);
 CREATE TABLE playlist_musicas(fk_musica INTEGER NOT NULL, fk_playlist INTEGER NOT NULL, ordem INTEGER, FOREIGN KEY (fk_musica) REFERENCES musicas(id), foreign key (fk_playlist) references playlist(playlist_id) );
 CREATE TABLE playlist(id integer not null primary key, titulo STRING NOT NULL , data_apresentacao DATE);
+CREATE TABLE modelos(id INTEGER PRIMARY KEY, nome STRING NOT NULL, modelo TEXT NOT NULL, tipo STRING NOT NULL);
+-- DUMP DE DADOS
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE modelos(id INTEGER PRIMARY KEY, nome STRING NOT NULL, modelo TEXT NOT NULL, tipo STRING NOT NULL);
+INSERT INTO "modelos" VALUES(1,'Nova Música','<html><head><meta="utf-8"><style>body{color:#fff; background-color: #000; } h1, h2 { color:orange; }</style></head><body><h1 contenteditable="true">Teste</h1><div contenteditable="true"><p>testando</p></div></body></html>','default');
+COMMIT;
