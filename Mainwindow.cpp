3a725947631586749ea60802a3e1b82cccf93e4f
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2018-07-14 16:06:32 -0300

    Adiciona SQL do programa da música

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -208,92 +208,122 @@ void MainWindow::on_actionConectar_triggered()
 void MainWindow::on_actionSalvar_SYSEX_triggered()
 {
     if(ui->listWidget->selectedItems().length() <= 0){
-        //esse workflow está ruim. nem deveria mexer nas partes sem uma música selecionada
-        //ao selecionar vai apagar as mudanças
         QMessageBox msg;
         msg.setText("Selecione uma música da lista");
         msg.exec();
         return;
     }
 
-    //QString diretorio = QFileDialog::getExistingDirectory();
-    //QSettings *conf = new QSettings(diretorio.append("/teste.ini"), QSettings::IniFormat);
-    QString arquivoSysx = QString(configSysExDir).append("/" + ui->listWidget->selectedItems()[0]->text() + ".ini");
+    try
+    {
+        QString tmp("/tmp/QStage.ini");
+        QFile arquivoTemporario(tmp);
 
-    if(QFile(arquivoSysx).exists()){
-        QMessageBox::StandardButton resposta = QMessageBox::question(this, "Sobrescrever SYSX", "O arquivo SYSX já existe.\nSobrescrever?", QMessageBox::Yes | QMessageBox::No);
-        if(resposta != QMessageBox::Yes)
-            return;
-    }
+        int playlistId = ui->playlist->itemData(ui->playlist->currentIndex()).value<int>();
+        QListWidgetItem * selecionado = ui->listWidget->selectedItems()[0];
+        Musica * musica = selecionado->data(Qt::UserRole).value<Musica*>();
+        int musicaId = musica->musicaId;
 
-    QSettings *conf = new QSettings(arquivoSysx, QSettings::IniFormat);
-    conf->beginGroup("PerformanceCommon");
-    conf->setValue("nome",ui->perfName->text());
-    conf->setValue("origem",ui->perfOrigem->currentIndex());
-    conf->setValue("efeito",ui->perfEfeito->currentIndex() );
-    conf->setValue("param1",ui->perfParam->value());
-    conf->setValue("param2",ui->perfParam_2->value());
-    conf->setValue("param3",ui->perfParam_3->value());
-    conf->setValue("param4",ui->perfParam_4->value());
-    conf->setValue("param5",ui->perfParam_5->value());
-    conf->setValue("param6",ui->perfParam_6->value());
-    conf->setValue("param7",ui->perfParam_7->value());
-    conf->setValue("param8",ui->perfParam_8->value());
-    conf->setValue("param9",ui->perfParam_9->value());
-    conf->setValue("param10",ui->perfParam_10->value());
-    conf->setValue("param11",ui->perfParam_11->value());
-    conf->setValue("param12",ui->perfParam_12->value());
-    conf->setValue("oa",ui->perfOA->currentIndex());
-    conf->setValue("msl",ui->perfMSL->value());
-    conf->setValue("csl",ui->perfCSL->value());
-    conf->setValue("rsl",ui->perfperfRSL->value());
-    conf->setValue("ctrlSrc1",ui->perfCtrlSrc1->currentIndex());
-    conf->setValue("ctrlDepth1",ui->perfCtrlDepth1->value());
-    conf->setValue("ctrlSrc2",ui->perfCtrlSrc2->currentIndex());
-    conf->setValue("ctrlDepth2",ui->perfCtrlDepth2->value());
-    conf->setValue("chorusLevel",ui->perfChorusLevel->value());
-    conf->setValue("chorusRate",ui->perfChorusRate->value());
-    conf->setValue("chorusDepth",ui->perfChorusDepth->value());
-    conf->setValue("chorusPreDelay",ui->perfChorusPreDelay->value());
-    conf->setValue("chorusFeedback",ui->perfChorusFeedback->value());
-    conf->setValue("chorusOut",ui->perfChorusOut->currentIndex());
-    conf->setValue("reverbType",ui->perfReverbType->currentIndex());
-    conf->setValue("reverbLevel",ui->perfReverbLevel->value());
-    conf->setValue("reverbTime",ui->perfReverbTime->value());
-    conf->setValue("reverbHFDamp",ui->perfReverbHFDamp->currentIndex());
-    conf->setValue("reverbDelayFeedback",ui->perfReverbDelayFeedback->value());
-    conf->setValue("tempo",ui->perfTempo->value());
-    conf->setValue("intervaloNotas",ui->perfIntervaloNotas->isChecked() ? 1 : 0);
-    conf->setValue("modo",ui->perfMode->currentIndex());
-    conf->endGroup();
+        if(musica->programa.length() > 0){
+            QMessageBox::StandardButton resposta = QMessageBox::question(this, "Sobrescrever SYSX", "O arquivo SYSX para "+ musica->titulo +" já existe.\nSobrescrever?", QMessageBox::Yes | QMessageBox::No);
+            if(resposta != QMessageBox::Yes)
+                return;
+        }
 
-    for(int i=0 ; i<=15; i++){
-        PartTab *tab = tabParts[i];
-        conf->beginGroup(QString("Parte%1").arg(i));
-
-        conf->setValue("nome", tab->getPatch()->getNome() );
-        conf->setValue("categoria", tab->getPatch()->getCategoria() );
-        conf->setValue("categoriaPai", tab->getPatch()->getCategoriaPai() );
-        conf->setValue("groupId", tab->getPatch()->getGroupId() );
-        conf->setValue("groupType", tab->getPatch()->getGroupType() );
-        conf->setValue("number", tab->getPatch()->getNumber() );
-
-        conf->setValue("afinacaoBruta", tab->getAfinacaoBruta() );
-        conf->setValue("afinacaoFina", tab->getAfinacaoFina() );
-        conf->setValue("canalMidi", tab->getCanalMidi() );
-        conf->setValue("chorusLevel", tab->getChorusLevel() );
-        conf->setValue("mixEfxSendLevel", tab->getMixEfxLevel() );
-        conf->setValue("oitava", tab->getOitava() );
-        conf->setValue("pan", tab->getPan() );
-        conf->setValue("regiaoMax", tab->getRegiaoMax() );
-        conf->setValue("regiaoMin", tab->getRegiaoMin() );
-        conf->setValue("reverbLevel", tab->getReverbLevel() );
-        conf->setValue("saida", tab->getSaida() );
-        conf->setValue("volume", tab->getVolume() );
-        conf->setValue("localOn", tab->isLocalOn() );
-
+        //salva no arquivo temporário
+        QSettings *conf = new QSettings(arquivoTemporario.fileName(), QSettings::IniFormat);
+        conf->beginGroup("PerformanceCommon");
+        conf->setValue("nome",ui->perfName->text());
+        conf->setValue("origem",ui->perfOrigem->currentIndex());
+        conf->setValue("efeito",ui->perfEfeito->currentIndex() );
+        conf->setValue("param1",ui->perfParam->value());
+        conf->setValue("param2",ui->perfParam_2->value());
+        conf->setValue("param3",ui->perfParam_3->value());
+        conf->setValue("param4",ui->perfParam_4->value());
+        conf->setValue("param5",ui->perfParam_5->value());
+        conf->setValue("param6",ui->perfParam_6->value());
+        conf->setValue("param7",ui->perfParam_7->value());
+        conf->setValue("param8",ui->perfParam_8->value());
+        conf->setValue("param9",ui->perfParam_9->value());
+        conf->setValue("param10",ui->perfParam_10->value());
+        conf->setValue("param11",ui->perfParam_11->value());
+        conf->setValue("param12",ui->perfParam_12->value());
+        conf->setValue("oa",ui->perfOA->currentIndex());
+        conf->setValue("msl",ui->perfMSL->value());
+        conf->setValue("csl",ui->perfCSL->value());
+        conf->setValue("rsl",ui->perfperfRSL->value());
+        conf->setValue("ctrlSrc1",ui->perfCtrlSrc1->currentIndex());
+        conf->setValue("ctrlDepth1",ui->perfCtrlDepth1->value());
+        conf->setValue("ctrlSrc2",ui->perfCtrlSrc2->currentIndex());
+        conf->setValue("ctrlDepth2",ui->perfCtrlDepth2->value());
+        conf->setValue("chorusLevel",ui->perfChorusLevel->value());
+        conf->setValue("chorusRate",ui->perfChorusRate->value());
+        conf->setValue("chorusDepth",ui->perfChorusDepth->value());
+        conf->setValue("chorusPreDelay",ui->perfChorusPreDelay->value());
+        conf->setValue("chorusFeedback",ui->perfChorusFeedback->value());
+        conf->setValue("chorusOut",ui->perfChorusOut->currentIndex());
+        conf->setValue("reverbType",ui->perfReverbType->currentIndex());
+        conf->setValue("reverbLevel",ui->perfReverbLevel->value());
+        conf->setValue("reverbTime",ui->perfReverbTime->value());
+        conf->setValue("reverbHFDamp",ui->perfReverbHFDamp->currentIndex());
+        conf->setValue("reverbDelayFeedback",ui->perfReverbDelayFeedback->value());
+        conf->setValue("tempo",ui->perfTempo->value());
+        conf->setValue("intervaloNotas",ui->perfIntervaloNotas->isChecked() ? 1 : 0);
+        conf->setValue("modo",ui->perfMode->currentIndex());
         conf->endGroup();
+
+        for(int i=0 ; i<=15; i++){
+            PartTab *tab = tabParts[i];
+            conf->beginGroup(QString("Parte%1").arg(i));
+
+            conf->setValue("nome", tab->getPatch()->getNome() );
+            conf->setValue("categoria", tab->getPatch()->getCategoria() );
+            conf->setValue("categoriaPai", tab->getPatch()->getCategoriaPai() );
+            conf->setValue("groupId", tab->getPatch()->getGroupId() );
+            conf->setValue("groupType", tab->getPatch()->getGroupType() );
+            conf->setValue("number", tab->getPatch()->getNumber() );
+
+            conf->setValue("afinacaoBruta", tab->getAfinacaoBruta() );
+            conf->setValue("afinacaoFina", tab->getAfinacaoFina() );
+            conf->setValue("canalMidi", tab->getCanalMidi() );
+            conf->setValue("chorusLevel", tab->getChorusLevel() );
+            conf->setValue("mixEfxSendLevel", tab->getMixEfxLevel() );
+            conf->setValue("oitava", tab->getOitava() );
+            conf->setValue("pan", tab->getPan() );
+            conf->setValue("regiaoMax", tab->getRegiaoMax() );
+            conf->setValue("regiaoMin", tab->getRegiaoMin() );
+            conf->setValue("reverbLevel", tab->getReverbLevel() );
+            conf->setValue("saida", tab->getSaida() );
+            conf->setValue("volume", tab->getVolume() );
+            conf->setValue("localOn", tab->isLocalOn() );
+
+            conf->endGroup();
+        }
+
+
+        arquivoTemporario.flush();
+
+//        qDebug() << arquivoTemporario.fileName();
+        arquivoTemporario.close();
+//        arquivoTemporario.open();
+        QFile arquivo2(tmp);
+        arquivo2.open(QFile::ReadOnly);
+        QTextStream fluxo(&arquivo2);
+        //persiste do arquivo no banco de dados
+        SQLite::Database db(QString(qstageDir + "/qstage.db").toUtf8().data(), SQLite::OPEN_READWRITE);
+        SQLite::Statement   query(db, "UPDATE musicas SET programa = ? WHERE musica_id = ?");
+        query.bind(1, fluxo.readAll().toUtf8().data() );
+        query.bind(2, musicaId);
+        query.exec();
+        arquivo2.close();
+        this->on_actionAtualizar_Playlists_triggered();
     }
+    catch (std::exception& e)
+    {
+        QMessageBox::warning(this,"Erro ao Remover Música", e.what());
+        qDebug() << "exception: " << e.what();
+    }
+
 }
 
 void MainWindow::on_actionAbrir_SYSEX_triggered()
@@ -306,7 +336,15 @@ void MainWindow::on_actionAbrir_SYSEX_triggered()
 
     //QSettings *conf = new QSettings("/tmp/teste.ini", QSettings::IniFormat);
 //    QSettings *conf = new QSettings(QString(configSysExDir).append("/" + ui->listWidget->selectedItems()[0]->text() + ".ini"), QSettings::IniFormat);
-    QSettings *conf = new QSettings( musica->programa , QSettings::IniFormat);
+
+    QTemporaryFile arquivoTemporario;
+    if(!arquivoTemporario.open())
+        qDebug() << "erro ao abrir arquivo temporário";
+
+    QTextStream fluxo(&arquivoTemporario);
+    fluxo << musica->programa << endl;
+    fluxo.flush();
+    QSettings *conf = new QSettings( arquivoTemporario.fileName() , QSettings::IniFormat);
     conf->beginGroup("PerformanceCommon");
 
     ui->perfName->setText(conf->value("nome").toString());
