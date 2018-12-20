e6d128024173be725504cb5dd351ded106a84a0e
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2018-12-20 11:50:43 -0200

    Adiciona tabela de waveforms

--- qstage.sql
+++ qstage.sql
@@ -1,12 +1,6 @@
 -- DB QStage
-CREATE TABLE musicas( musica_id INTEGER NOT NULL PRIMARY KEY, titulo STRING NOT NULL, html TEXT, programa TEXT, artista STRING);
-CREATE TABLE playlist_musicas(fk_musica INTEGER NOT NULL, fk_playlist INTEGER NOT NULL, ordem INTEGER, FOREIGN KEY (fk_musica) REFERENCES musicas(id), foreign key (fk_playlist) references playlist(playlist_id) );
-CREATE TABLE playlist(id integer not null primary key, titulo STRING NOT NULL , data_apresentacao DATE);
-CREATE TABLE modelos(id INTEGER PRIMARY KEY, nome STRING NOT NULL, modelo TEXT NOT NULL, tipo STRING NOT NULL);
--- DUMP DE DADOS
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
--- CREATE TABLE modelos(id INTEGER PRIMARY KEY, nome STRING NOT NULL, modelo TEXT NOT NULL, tipo STRING NOT NULL);
-INSERT INTO "modelos" VALUES(2,'Nova Música','<html><head><meta="utf-8"><style>body{color:#fff; background-color: #000; } h1, h2 { color:orange; }</style></head><body><h1>Teste</h1><div><p>testando</p></div></body></html>','default');
-INSERT INTO "modelos" VALUES(3,'Modelo 2','<body><h1>Modelo #2</h2><iframe src="http://localhost"></body>','Teste');
-COMMIT;
+CREATE TABLE IF NOT EXISTS  musicas( musica_id INTEGER NOT NULL PRIMARY KEY, titulo STRING NOT NULL, html TEXT, programa TEXT, artista STRING);
+CREATE TABLE IF NOT EXISTS  playlist_musicas(fk_musica INTEGER NOT NULL, fk_playlist INTEGER NOT NULL, ordem INTEGER, FOREIGN KEY (fk_musica) REFERENCES musicas(id), foreign key (fk_playlist) references playlist(playlist_id) );
+CREATE TABLE IF NOT EXISTS  playlist(id integer not null primary key, titulo STRING NOT NULL , data_apresentacao DATE);
+CREATE TABLE IF NOT EXISTS  modelos(id INTEGER PRIMARY KEY, nome STRING NOT NULL, modelo TEXT NOT NULL, tipo STRING NOT NULL);
+CREATE TABLE IF NOT EXISTS waveforms(id INTEGER PRIMARY KEY, nome STRING NOT NULL, groupType INTEGER NOT NULL, groupId INTEGER NOT NULL, waveGroup STRING NOT NULL, oneShot INTEGER NOT NULL, number INTEGER NOT NULL);
