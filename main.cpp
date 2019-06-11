3f262c9e61a55c870af35e72fa6e732f2aa85335
Marcel Bonnet <marcelbonnet@gmail.com>
2019-06-11 20:43:55 -0300

    adiciona Splash Screen

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include "Mainwindow.h"
 #include <QApplication>
-
+#include <QSplashScreen>
 
 //#include <smf.h>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QApplication app(argc, argv);
+    QPixmap pixmap(":/icones/QStage.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+    splash.showMessage("Carregando", Qt::AlignLeft, Qt::yellow);
+    app.processEvents();
+
+    MainWindow window;
+    window.show();
+    splash.finish(&window);
+    return app.exec();
 
     //smf_load("/studio/OLD/Estudo/Paul\ McCartney\ MIDIs/LondonTown.mid");
-
-
-
-    return a.exec();
 }
