004570dc9fa65c88975c17d03328f60ef1dea6ab
Marcel Bonnet <marcelbonnet@gmail.com>
2019-06-29 18:57:20 -0300

    LIMPEZA DE CÓDIGO

--- PerformancePart.h
+++ PerformancePart.h
@@ -7,44 +7,11 @@ class PerformancePart
 {
 public:
 
-    enum Function {
-        ReceiveSwitch,
-        MIDIChannel,
-        PatchGroupType,
-        PatchGroupID,
-        PatchNumber,
-        PartLevel,
-        PartPan,
-        PartCoarseTune,
-        PartFineTune,
-        OutputAssign,
-        MixEFXSendLevel,
-        ChorusSendLevel,
-        ReverbSendLevel,
-        ReceiveProgramChangeSwitch,
-        ReceiveVolumeSwitch,
-        ReceiveHold1Switch,
-        KeyboardRangeLower,
-        KeyboardRangeUpper,
-        OctaveShift,
-        LocalSwitch,
-        TransmitSwitch,
-        TransmitBankSelectGroup,
-        TransmitVolume,
-    };
+
 
     PerformancePart();
-    PerformancePart(Function func, int part);
-
-    int address = 0x0000;
-
-    bool is2ByteData = false;
-
-    QString functionName;
 
 
-private:
-    QMap<Function,QString> functionMap;
 };
 
 #endif // PERFORMANCEPART_H
